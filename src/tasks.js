7212dfccd90bb5f5c0f414b748da37899867872f
Supun Peiris <supunvidarshana2004@gmail.com>
2026-09-24 20:43:47 +0530

    feat: add initial CampusEats task list

---
+++ src/tasks.js
@@ -0,0 +1,8 @@
+// CampusEats task list
+
+const tasks = [
+  "Design the CRM layout and menu screen",
+  "Build the C# orders API",
+  "Add JWT user login"
+];
+console.log(`CampusEats has ${tasks.length} open tasks`);
